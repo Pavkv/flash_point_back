72a488a9c80685e3f5c88ba64649472e00fe4e1b
Pavel Zobov <pavelzobov123@gmail.com>
2025-05-23 09:43:50 -0400

    feat: Add newsApi backend support

--- controllers/getArticles.js
+++ controllers/getArticles.js
@@ -1,12 +1,29 @@
-const User = require('../models/user');
-const NotFoundError = require("../utils/Errors/NotFoundError");
+const axios = require('axios');
+
+const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 module.exports = (req, res, next) => {
-  User.findById(req.user._id)
-      .orFail(() => new NotFoundError('User not found'))
-      .populate('savedArticles')
-      .then((user) => {
-        res.send({ data: user.savedArticles });
-      })
-      .catch(next);
+    const { q } = req.query;
+
+    axios.get('https://newsapi.org/v2/everything', {
+        params: {
+            q,
+            apiKey: NEWS_API_KEY,
+            language: 'en',
+        },
+    }).then((response) => {
+        const articles = response.data.articles
+            .filter((article) => article.urlToImage && article.author)
+            .map((article) => ({
+                title: article.title,
+                author: article.author,
+                description: article.description,
+                imageUrl: article.urlToImage,
+                url: article.url,
+                date: article.publishedAt,
+                keyword: q.charAt(0).toUpperCase() + q.slice(1),
+                isSaved: false,
+            }));
+        res.send({ articles });
+    }).catch(next)
 };
